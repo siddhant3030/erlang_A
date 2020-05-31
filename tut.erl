cf20cdf849656f6e7cf8c22cd68d6d081c5ec8b8
siddhant3030 <ssiddhant3030@gmail.com>
2020-05-31 18:23:32 +0530

    erland expressions

--- tut.erl
+++ tut.erl
@@ -2,10 +2,19 @@
 
 -import(string, [len/1, concat/2, chr/2, substr/3, str/2, to_lower/1, to_upper/2]).
 
--export([hello_world/0, add/2, atom_stuff/0]).
+-export([hello_world/0, add/2, atom_stuff/0, main/0]).
 
 hello_world() -> 
     io:fwrite("Hello World\n").
+
+main() -> compare(4, 4.0).
+
+compare(A, B) -> 
+    A =:= B,
+    A =/= B,
+    A == B,
+    A /= B.
+
 
 add(A, B) -> 
     atom_stuff(),
