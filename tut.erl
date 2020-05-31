f6c83cd61a0eb8c93c114b7e39b5b381ecfad6a3
siddhant3030 <ssiddhant3030@gmail.com>
2020-05-31 15:05:52 +0530

    erl functions

--- tut.erl
+++ tut.erl
@@ -2,7 +2,15 @@
 
 -import(string, [len/1, concat/2, chr/2, substr/3, str/2, to_lower/1, to_upper/2]).
 
--export([hello_world/0]).
+-export([hello_world/0, add/2, atom_stuff/0]).
 
 hello_world() -> 
     io:fwrite("Hello World\n").
+
+add(A, B) -> 
+    atom_stuff(),
+    hello_world(),
+    A + B.
+
+atom_stuff() ->
+    'An Atom'.
