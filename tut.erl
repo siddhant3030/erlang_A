5bee2db9bbf11addfe6a376b4670eb4c503f1c7b
siddhant3030 <ssiddhant3030@gmail.com>
2020-05-31 18:32:11 +0530

    case expression

--- tut.erl
+++ tut.erl
@@ -2,7 +2,7 @@
 
 -import(string, [len/1, concat/2, chr/2, substr/3, str/2, to_lower/1, to_upper/2]).
 
--export([hello_world/0, add/2, atom_stuff/0, main/0]).
+-export([hello_world/0, add/2, atom_stuff/0, main/0, mains/0]).
 
 hello_world() -> 
     io:fwrite("Hello World\n").
@@ -14,6 +14,16 @@ compare(A, B) ->
     A =/= B,
     A == B,
     A /= B.
+
+mains() -> 
+    say_hello(french).
+
+say_hello(X) -> 
+    case X of
+        french -> 'Bon';
+        german -> 'hey';
+        english -> 'hello'
+    end.
 
 
 add(A, B) -> 
