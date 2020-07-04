a9dff2bd116236639e16ad3d17a38125b05d3a86
siddhant3030 <ssiddhant3030@gmail.com>
2020-07-04 18:39:53 +0530

    basic thing added

--- hedgehog/apps/streamer/lib/streamer/binance.ex
+++ hedgehog/apps/streamer/lib/streamer/binance.ex
@@ -7,7 +7,28 @@ defmodule Streamer.Binance do
   end
 
   def handle_frame({type, msg}, state) do
-    IO.puts("Received Message - Type: #{inspect(type)} -- Message: #{inspect(msg)}")
+    case Jason.decode(msg) do
+      {:ok, event} -> handle_event(event, state)
+      {:error, _} -> throw("Unable to parse msg #{msg}")
+    end
+
     {:ok, state}
+  end
+
+  def handle_event(%{"e" => "trade"} = event, state) do
+    trade_event = %Streamer.Binance.TradeEvent{
+      :event_type => event["e"],
+      :event_time => event["E"],
+      :symbol => event["s"],
+      :trade_id => event["a"],
+      :price => event["p"],
+      :quantity => event["q"],
+      :buyer_order_id => event["f"],
+      :seller_order_id => event["l"],
+      :trade_time => event["T"],
+      :buyer_market_maker => event["m"]
+    }
+
+    IO.inspect(trade_event, label: "Trade event received: ")
   end
 end
