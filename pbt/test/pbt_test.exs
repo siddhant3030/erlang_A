6747eb66d72c330a863e5572f50acca3aa801c90
siddhant3030 <ssiddhant3030@gmail.com>
2020-09-17 18:16:25 +0530

    new test added

--- pbt/test/pbt_test.exs
+++ pbt/test/pbt_test.exs
@@ -22,4 +22,20 @@ defmodule PbtTest do
   def my_type() do
     term()
   end
+
+  def biggest([head | tail]) do
+    biggest(tail, head)
+  end
+
+  defp biggest([], max) do
+    max
+  end
+
+  defp biggest([head | tail], max) when head > max do
+    biggest(tail, head)
+  end
+
+  defp biggest([head | tail], max) when head < max do
+    biggest(tail, max)
+  end
 end
