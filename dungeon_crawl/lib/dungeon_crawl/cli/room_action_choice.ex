159f29c40cd46ca36831506ae7238e11ceba7d27
siddhant3030 <ssiddhant3030@gmail.com>
2020-10-01 16:23:24 +0530

    room action choice

--- dungeon_crawl/lib/dungeon_crawl/cli/room_action_choice.ex
+++ dungeon_crawl/lib/dungeon_crawl/cli/room_action_choice.ex
@@ -1,6 +1,6 @@
 defmodule DungeonCrawl.CLI.RoomActionsChoice do
   alias Mix.Shell.IO, as: Shell
-  import DungeonCrawl.CLI.BaseCommands
+  alias DungeonCrawl.CLI.BaseCommands
 
   def start(room) do
     room_actions = room.actions
