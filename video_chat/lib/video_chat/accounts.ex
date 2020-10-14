2786088c8f96cfcd1fd6113ec4386d37f5fa2c4b
siddhant3030 <ssiddhant3030@gmail.com>
2020-10-14 20:20:31 +0530

    changes around context

--- video_chat/lib/video_chat/accounts.ex
+++ video_chat/lib/video_chat/accounts.ex
@@ -1,26 +1,23 @@
-# defmodule VideoChat.Accounts do
+defmodule VideoChat.Accounts do
 
-#   alias VideoChat.Accounts.User
+  alias VideoChat.Accounts.User
 
-#   def list_users do
-#     [
-#       %User{author: "1", content: "br", title: "tame impala"},
-#       %User{author: "2", content: "bru", title: "tame impala"},
-#       %User{author: "3", content: "bruc", title: "tame impala"},
-#       %User{author: "4", content: "brucee", title: "tame impala"},
-#       %User{author: "5", content: "bruceee", title: "tame impala"},
-#       %User{author: "6", content: "bruceeee", title: "tame impala"},
-#       %User{author: "7", content: "bruceeeee", title: "tame impala"}
-#     ]
-#   end
+  def list_users do
+    [
+      %User{​id:​ ​"​​1"​, ​name:​ ​"​​Josée"​, ​username:​ ​"​​josevalim"​},
+      %User{​id:​ ​"​​2"​, ​name:​ ​"​​Joséeee"​, ​username:​ ​"​​josevalim"​},
+      %User{​id:​ ​"​​3"​, ​name:​ ​"​​Joséeeee"​, ​username:​ ​"​​josevalim"​},
+      %User{​id:​ ​"​​4"​, ​name:​ ​"​​Joséeeeee"​, ​username:​ ​"​​josevalim"​}
+    ]
+  end
 
-#   def get_user(author) do
-#     Enum.find(list_users(), fn map -> map.author == author end)
-#   end
+  def get_user(author) do
+    Enum.find(list_users(), fn map -> map.author == author end)
+  end
 
-#   def get_user_by(params) do
-#     Enum.find(list_users(), fn map ->
-#       Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-#     end)
-#   end
-# end
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
+end
