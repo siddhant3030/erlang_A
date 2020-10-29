8d5be99d1c8fe4fcc8728aa0ca9571570dc0b7c6
siddhant3030 <ssiddhant3030@gmail.com>
2020-10-29 18:52:01 +0530

    more queries added

--- auth/.iex.exs
+++ auth/.iex.exs
@@ -16,4 +16,8 @@ alias Auth.MusicDb.{Artist, Album, Track, Genre}
 # q = from a in "artists", where: not is_nil(a.name), select: [:id, :name]
 # # date comparison - this finds artists added more than 1 year ago
 # q = from a in "artists", where: a.inserted_at < ago(1, "year"), select: [:id, :name]
-1
+
+# q = from t in "tracks", select: [t.album_id, t.title, t.index, t.duration], order_by: [t.album_id, t.index]
+
+# group by example
+# q = from t in "tracks", select: [t.album_id, sum(t.duration)], group_by: t.album_id
