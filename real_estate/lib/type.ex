e73c9ceb7a18e3aa8b458e708d736d051f0726ee
siddhant3030 <ssiddhant3030@gmail.com>
2020-11-04 18:13:08 +0530

    ecto cast

--- real_estate/lib/type.ex
+++ real_estate/lib/type.ex
@@ -12,4 +12,13 @@ defmodule DateTimeUnix do
   def dump(term), do: Ecto.Type.dump(:datetime, term)
 
   def load(term), do: Ecto.Type.load(:datetime, term)
+
+  def cast("Date(" <> rest) do
+    with {unix, ")"} <- Integer.parse(rest),
+    {:ok, datetime} <- DateTime.from_unix(unix) do
+    {:ok, datetime} else
+    _ -> :error end
+    end
+
+  def cast(%DateTime{} = datetime), do: {:ok, datetime} def cast(_other), do: :error
 end
