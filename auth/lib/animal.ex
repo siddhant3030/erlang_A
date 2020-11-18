f9e5061be34169afe47bca5868f0d7db9acb8092
siddhant3030 <ssiddhant3030@gmail.com>
2020-11-18 22:22:23 +0530

    protocols

---
+++ auth/lib/animal.ex
@@ -0,0 +1,30 @@
+defprotocol Animal do
+  def greet(arg)
+  def speak(arg)
+  def warn(arg)
+end
+
+defmodule Dog do
+  @enforce_keys [:name]
+  alias __MODULE__
+  defstruct name: ""
+
+  def new(name) do
+    %Dog{name: name}
+  end
+
+  defimpl Animal, for: Dog do
+    def warn(_), do: ("growl!")
+    def speak(_), do: ("gsasassarowl!")
+    def greet(_), do: ("hello")
+  end
+end
+
+# Compiling 1 file (.ex)
+# Generated auth app
+# :ok
+# iex(5)> max = Dog.new("max")
+# %Dog{name: "max"}
+# iex(6)> max |> Animal.speak
+# "gsasassarowl!"
+# iex(7)>
