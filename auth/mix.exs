8bc1314d6366057997ea0a9211bea1d485e8d9be
siddhant3030 <ssiddhant3030@gmail.com>
2020-12-01 22:53:50 +0530

    new testing module

--- auth/mix.exs
+++ auth/mix.exs
@@ -50,8 +50,6 @@ defmodule Auth.MixProject do
       {:comeonin, "~> 5.3"},
       {:ex_doc, "~> 0.13"},
       {:kaffy, "~> 0.9.0"},
-      {:money, "~> 1.8"},
-      {:ex_money, "~> 5.4"},
       {:nanoid, "~> 2.0.4"},
       {:muzak, "~> 1.0"}
     ]
