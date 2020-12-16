274a43bdda20a35fb5d656766704aae52b086bd2
siddhant3030 <ssiddhant3030@gmail.com>
2020-12-16 17:10:03 +0530

    enum practice

--- auth/lib/saas.ex
+++ auth/lib/saas.ex
@@ -32,3 +32,14 @@
 #     |> unique_constraint(:state_id, name: :unique_zone_and_country)
 #   end
 # end
+
+# iex(1)> Enum.at(10..20, 3)
+# 13
+# iex(2)> Enum.take(list, 2)
+# ** (CompileError) iex:2: undefined function list/0
+#     (stdlib 3.13.2) lists.erl:1358: :lists.mapfoldl/3
+# iex(2)> list = Enum.to_list 1..5
+# [1, 2, 3, 4, 5]
+# iex(3)> Enum.take(list, 2)
+# [1, 2]
+# iex(4)>
