982e315dd668eb4413c391a8d83c3d23dbeb28f0
siddhant3030 <ssiddhant3030@gmail.com>
2020-12-18 20:43:08 +0530

    new fizzbuzz idea

--- auth/lib/fizzbuzz3.ex
+++ auth/lib/fizzbuzz3.ex
@@ -1,4 +1,4 @@
-defmodule FizzBuzz do
+defmodule FizzBuzzB do
   def upto(n) when n > 0, do: 1..n |> Enum.map(&fizzbuzz/1)
   defp fizzbuzz(n), do: _fizzword(n, rem(n, 3), rem(n, 5))
   defp _fizzword(_n, 0, 0), do: "FizzBuzz"
