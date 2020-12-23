a64c36538cfa17e063d66bb4516c95396f48a840
siddhant3030 <ssiddhant3030@gmail.com>
2020-12-23 19:47:58 +0530

    transformation: fetch from github

--- issues/lib/cli.ex
+++ issues/lib/cli.ex
@@ -2,24 +2,40 @@ defmodule Issues.CLI do
   @default_count 4
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args(argv)
+    |> process
   end
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    |> args_to_internal_representation()
+  end
 
-    case parse do
-      {[help: true], _, _} ->
-        :help
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """)
 
-      {_, [user, project, count], _} ->
-        {user, project, String.to_integer(count)}
+    System.halt(0)
+  end
 
-      {_, [user, project], _} ->
-        {user, project, @default_count}
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
 
-      _ ->
-        :help
-    end
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  def args_to_internal_representation(_) do
+    # bad arg or --help :help
   end
 end
