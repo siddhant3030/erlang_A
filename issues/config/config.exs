f3356bc05b5b333677d1edd0fee1d08573c7e062
siddhant3030 <ssiddhant3030@gmail.com>
2020-12-27 18:07:21 +0530

    config changes

--- issues/config/config.exs
+++ issues/config/config.exs
@@ -1,3 +1,4 @@
 use Mix.Config
 
 config :issues, github_url: "https://api.github.com"
+import_config "#{Mix.env()}.exs"
--- issues/lib/github_issues.ex
+++ issues/lib/github_issues.ex
@@ -1,12 +1,14 @@
 defmodule Issues.GithubIssues do
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
+  # use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
     issues_url(user, project) |> HTTPoison.get(@user_agent) |> handle_response
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
