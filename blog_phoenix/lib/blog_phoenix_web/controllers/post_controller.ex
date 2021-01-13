8ba36b58e1c205b822ed060591a91db2407b8743
siddhant3030 <ssiddhant3030@gmail.com>
2021-01-13 23:38:21 +0530

    plug added

--- blog_phoenix/lib/blog_phoenix_web/controllers/post_controller.ex
+++ blog_phoenix/lib/blog_phoenix_web/controllers/post_controller.ex
@@ -2,7 +2,9 @@ defmodule BlogPhoenixWeb.PostController do
   use BlogPhoenixWeb, :controller
 
   alias BlogPhoenix.Blog
+  alias BlogPhoenix.Blog.Comment
   alias BlogPhoenix.Blog.Post
+  plug :scrub_params, "comment" when action in [:add_comment]
 
   def index(conn, _params) do
     posts = Blog.list_posts()
@@ -58,5 +60,20 @@ defmodule BlogPhoenixWeb.PostController do
     conn
     |> put_flash(:info, "Post deleted successfully.")
     |> redirect(to: Routes.post_path(conn, :index))
+  end
+
+  def add_comment(conn, %{"comment" => comment_params, "post_id" => post_id}) do
+    changeset = Comment.changeset(%Comment{}, Map.put(comment_params, "post_id", post_id))
+    post = Post |> Repo.get(post_id) |> Repo.preload([:comments])
+
+    if changeset.valid? do
+      Repo.insert(changeset)
+
+      conn
+      |> put_flash(:info, "Comment added.")
+      |> redirect(to: post_path(conn, :show, post))
+    else
+      render(conn, "show.html", post: post, changeset: changeset)
+    end
   end
 end
