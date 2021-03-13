8ca50a800ad6d14423baac6f90d9d26c16586bf6
siddhant3030 <ssiddhant3030@gmail.com>
2021-03-13 15:15:27 +0530

    Idea for order number

--- auth/lib/auth/arists.ex
+++ auth/lib/auth/arists.ex
@@ -90,3 +90,73 @@ defmodule HealthyEats.Artists do
     Repo.delete(artist)
   end
 end
+
+# Example for idea
+# q = from t in Track, join: a in Album, on: t.album_id == a.id, where: t.index == 1
+# #Ecto.Query<from t0 in Auth.MusicDb.Track, join: a1 in Auth.MusicDb.Album,
+#  on: t0.album_id == a1.id, where: t0.index == 1>
+# iex(11)> Repo.all(q)
+# [debug] QUERY OK source="tracks" db=1.6ms queue=1.4ms idle=1004.7ms
+# SELECT t0."id", t0."title", t0."duration", t0."index", t0."number_of_plays", t0."inserted_at", t0."updated_at", t0."album_id" FROM "tracks" AS t0 INNER JOIN "albums" AS a1 ON t0."album_id" = a1."id" WHERE (t0."index" = 1) []
+# [
+#   %Auth.MusicDb.Track{
+#     __meta__: #Ecto.Schema.Metadata<:loaded, "tracks">,
+#     album: #Ecto.Association.NotLoaded<association :album is not loaded>,
+#     album_id: 1,
+#     duration: 544,
+#     id: 1,
+#     index: 1,
+#     inserted_at: ~N[2020-11-28 15:52:27],
+#     number_of_plays: 0,
+#     title: "So What",
+#     updated_at: ~N[2020-11-28 15:52:27]
+#   },
+#   %Auth.MusicDb.Track{
+#     __meta__: #Ecto.Schema.Metadata<:loaded, "tracks">,
+#     album: #Ecto.Association.NotLoaded<association :album is not loaded>,
+#     album_id: 2,
+#     duration: 1006,
+#     id: 6,
+#     index: 1,
+#     inserted_at: ~N[2020-11-28 15:52:27],
+#     number_of_plays: 0,
+#     title: "If I Were A Bell",
+#     updated_at: ~N[2020-11-28 15:52:27]
+#   },
+#   %Auth.MusicDb.Track{
+#     __meta__: #Ecto.Schema.Metadata<:loaded, "tracks">,
+#     album: #Ecto.Association.NotLoaded<association :album is not loaded>,
+#     album_id: 3,
+#     duration: 192,
+#     id: 11,
+#     index: 1,
+#     inserted_at: ~N[2020-11-28 15:52:27],
+#     number_of_plays: 0,
+#     title: "B Minor Waltz (for Ellaine)",
+#     updated_at: ~N[2020-11-28 15:52:27]
+#   },
+#   %Auth.MusicDb.Track{
+#     __meta__: #Ecto.Schema.Metadata<:loaded, "tracks">,
+#     album: #Ecto.Association.NotLoaded<association :album is not loaded>,
+#     album_id: 4,
+#     duration: 204,
+#     id: 18,
+#     index: 1,
+#     inserted_at: ~N[2020-11-28 15:52:27],
+#     number_of_plays: 0,
+#     title: "Come Rain Or Come Shine",
+#     updated_at: ~N[2020-11-28 15:52:27]
+#   },
+#   %Auth.MusicDb.Track{
+#     __meta__: #Ecto.Schema.Metadata<:loaded, "tracks">,
+#     album: #Ecto.Association.NotLoaded<association :album is not loaded>,
+#     album_id: 5,
+#     duration: 761,
+#     id: 27,
+#     index: 1,
+#     inserted_at: ~N[2020-11-28 15:52:28],
+#     number_of_plays: 0,
+#     title: "Anton's Ball",
+#     updated_at: ~N[2020-11-28 15:52:28]
+#   }
+# ]
