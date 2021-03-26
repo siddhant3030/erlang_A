5bfcc5f231183f91bc917cfeb963382354d6d72c
siddhant3030 <ssiddhant3030@gmail.com>
2021-03-26 11:14:51 +0530

    example added

--- auth/lib/auth/music_db/genre.ex
+++ auth/lib/auth/music_db/genre.ex
@@ -8,4 +8,19 @@ defmodule Auth.MusicDb.Genre do
 
     many_to_many :albums, Album, join_through: "albums_genres"
   end
+   # changeset = change(%Artist{name: "shivani"})
+
+  # new_album = Ecto.build_assoc(artist, :albums)
+  # %Auth.MusicDb.Album{
+  #   __meta__: #Ecto.Schema.Metadata<:built, "albums">,
+  #   artist: #Ecto.Association.NotLoaded<association :artist is not loaded>,
+  #   artist_id: 1,
+  #   genres: #Ecto.Association.NotLoaded<association :genres is not loaded>,
+  #   id: nil,
+  #   inserted_at: nil,
+  #   name: nil,
+  #   title: nil,
+  #   tracks: #Ecto.Association.NotLoaded<association :tracks is not loaded>,
+  #   updated_at: nil
+  # }
 end
