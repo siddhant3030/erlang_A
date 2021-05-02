6e95dfbf0b3400516e0954c854596e1004841ef5
siddhant3030 <ssiddhant3030@gmail.com>
2021-05-02 21:18:43 +0530

    channels added

---
+++ auth/lib/auth_web/channels/notification_channel.ex
@@ -0,0 +1,11 @@
+defmodule AuthWeb.NotificationChannel do
+  use Phoenix.Channel
+
+  def join(_topic, _payload, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("notification", _payload, socket) do
+    {:reply, {:ok, %{notification: "notifications"}}, socket}
+  end
+end
--- auth/lib/auth_web/channels/user_socket.ex
+++ auth/lib/auth_web/channels/user_socket.ex
@@ -2,7 +2,9 @@ defmodule AuthWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  channel "room:*", AuthWeb.RoomChannel
+  # channel "notification", AuthWeb.NotificationChannel
+  # channel "ping", AuthWeb.PingChannel
+  channel "room:lobby", HelloWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
