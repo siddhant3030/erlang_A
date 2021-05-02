9b68aa5fbf69e73c7e715dece3946f79a6381b20
siddhant3030 <ssiddhant3030@gmail.com>
2021-05-02 23:41:48 +0530

    channel changes

--- auth/lib/auth_web/channels/user_socket.ex
+++ auth/lib/auth_web/channels/user_socket.ex
@@ -4,7 +4,7 @@ defmodule AuthWeb.UserSocket do
   ## Channels
   # channel "notification", AuthWeb.NotificationChannel
   # channel "ping", AuthWeb.PingChannel
-  channel "room:lobby", HelloWeb.RoomChannel
+  channel "room", AuthWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
