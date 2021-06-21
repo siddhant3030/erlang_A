0b8d65dc244c93007da1d346d3be885d25e763bb
siddhant3030 <ssiddhant3030@gmail.com>
2021-06-21 13:01:02 +0530

    new resource

--- auth/lib/auth/contain.ex
+++ auth/lib/auth/contain.ex
@@ -1,3 +1,24 @@
-defmodule Auth.Contain do
-  defstruct [:name, :city, :phone, :map]
+defmodule Y do
+  def do_it(list1, list2) do
+    index_map = map_items_to_indexes(list2)
+    collect_matches(list1, index_map) |> Enum.reverse()
+  end
+
+  defp map_items_to_indexes(list, acc \\ %{}, index \\ 0)
+  defp map_items_to_indexes([], acc, _), do: acc
+  defp map_items_to_indexes([first | rest], acc, index) do
+    acc = Map.update(acc, first, [index], & [index | &1])
+    map_items_to_indexes(rest, acc, index + 1)
+  end
+
+  defp collect_matches(list, index_map, matches \\ [], index \\ 0)
+  defp collect_matches([], _, matches, _), do: matches
+  defp collect_matches([first | rest], index_map, matches, index) do
+    indexes = Map.get(index_map, first, []) |> Enum.reverse()
+    matches =
+      for match_index <- indexes, reduce: matches do
+        matches -> [[index, match_index] | matches]
+      end
+    collect_matches(rest, index_map, matches, index + 1)
+  end
 end
